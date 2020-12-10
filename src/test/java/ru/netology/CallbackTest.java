e787b4ab4c75cb4447a93a9f61cb1ff7fcb7c387
anaaveranova <skillet88jou@hotmail.com>
2020-12-10 19:32:28 +0300

    Добавление тестов

---
+++ src/test/java/ru/netology/CallbackTest.java
@@ -0,0 +1,32 @@
+package ru.netology;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CallbackTest {
+
+    @Test
+    void shouldSubmitRequestValid() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form. $("[data-test-id=name] input").setValue("Василий Горошкин");
+        form. $("[data-test-id=phone] input").setValue("+79270000000");
+        form. $("[data-test-id=agreement]").click();
+        form. $(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    void shouldSubmitRequestValidHyphen() {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form. $("[data-test-id=name] input").setValue("Иван Григошвили-оглы");
+        form. $("[data-test-id=phone] input").setValue("+79270000000");
+        form. $("[data-test-id=agreement]").click();
+        form. $(".button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
